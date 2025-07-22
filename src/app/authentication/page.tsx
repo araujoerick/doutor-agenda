9d28dfd8734b31c0b653c4741cf42c64b3c7aaa9
Erick Araújo <araujoerick.dev@gmail.com>
2025-07-22 11:54:06 -0300

    ✨ feat: implement session handling and redirection in Authentication and Dashboard pages

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,9 +1,21 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { auth } from "@/lib/auth";
 
 import SignInForm from "./components/sign-in-form";
 import SignUpForm from "./components/sign-up-form";
 
-const AuthenticationPage = () => {
+const AuthenticationPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session?.user) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="container mx-auto flex min-h-screen items-center justify-center px-8">
       <Tabs defaultValue="login" className="w-full max-w-sm">
