6e7f5db0a8763af284c061e6a5065a0ec757af6c
Erick Araújo <araujoerick.dev@gmail.com>
2025-07-26 23:12:44 -0300

    ✨ feat: add clinic data to user session and render it on sidebar

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,3 +1,7 @@
 import { createAuthClient } from "better-auth/react";
+import { customSessionClient } from "better-auth/client/plugins";
+import { auth } from "./auth";
 
-export const authClient = createAuthClient();
+export const authClient = createAuthClient({
+  plugins: [customSessionClient<typeof auth>()],
+});
